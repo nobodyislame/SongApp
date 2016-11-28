834403c5c23bcda2a9cbaee40877eebfe6f873e3
vikram1804.prasad@gmail.com <vikram1804.prasad@gmail.com>
2016-11-28 11:03:09 +0530

    UI Router module added and details tab updated

--- client/app.js
+++ client/app.js
@@ -1,7 +1,31 @@
 "use strict";
 
-angular.module("app", [ 
-	'app.controller', 
-	'app.directive',
-	'app.service'
-]);
+angular.module("app", [ 'ui.router',
+						'app.controller', 
+						'app.directive',
+						'app.service'
+				])
+
+	.config(function($stateProvider, $urlRouterProvider){
+		$stateProvider
+			.state('home', {
+				url:'/home',
+				templateUrl:'views/home.html',
+				controller:'HomeCtrl'
+			})
+
+			.state('details', {
+				url:'/details',
+				templateUrl:'views/details.html',
+				controller:'DetailsCtrl'
+			})
+
+			.state('playback', {
+				url:'/playback',
+				templateUrl:'views/playback.html',
+				controller:'PlaybackCtrl'
+			});
+
+		$urlRouterProvider.otherwise('home');
+
+	})
--- client/controllers/controllers.js
+++ client/controllers/controllers.js
@@ -1,5 +1,5 @@
 angular.module('app.controller',[])
-	.controller('songCtrl', ['$scope','$http', 'Song',function($scope, $http, Song){
+	.controller('HomeCtrl', ['$scope','$http', 'Song',function($scope, $http, Song){
 		
 		$scope.editmode = false;
 
@@ -58,5 +58,43 @@ angular.module('app.controller',[])
 			console.log($scope.newsong);
 		}
 
+		$scope.cancel =function(){
+			$scope.newsong = {};
+			$scope.editmode = !$scope.editmode;
+			$scope.loadPage();
+		}
+
 		$scope.loadPage();
+	}])
+
+	.controller('PlaybackCtrl', function($scope){
+		console.log("in playback control");
+	})
+
+	.controller('DetailsCtrl', ['$scope', 'Song', function($scope, Song){
+		
+		Song.get(function(err, result){
+			if(err){
+				console.log(err);
+			}
+
+			$scope.data = result;
+
+			$scope.details = {};
+
+			console.log($scope.details);
+			for(var i=0;i<$scope.data.length;i++){
+				if($scope.details==={}){
+					$scope.details[$scope.data[i].artist] = 1;
+				}
+
+				if($scope.details[$scope.data[i].artist]===undefined || $scope.details[$scope.data[i].artist]===null){
+					$scope.details[$scope.data[i].artist] = 1;
+				}
+
+				else{
+					$scope.details[$scope.data[i].artist]+=1;
+				}
+			}
+		});
 	}]);
